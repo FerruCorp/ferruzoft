910fe4367986491fba707292a93f35d0574e79a9
Jean Claude <jesquerraferruzo@gmail.com>
2016-03-17 08:32:55 -0500

    Selenium C#

---
+++ GoogleSearch.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace tarea.test
+{
+    class GoogleSearch
+    {
+
+        private static IWebDriver driver = null;
+        private static String url = "http://www.google.com.pe";
+        private static Util util = new Util();
+
+        private static void doSearch(String searchQuery, String[] arrPages)
+        {
+
+            driver.Navigate().GoToUrl(url);
+
+            IWebElement txtSearch = driver.FindElement(By.Name("q"));
+
+            txtSearch.SendKeys(searchQuery);
+
+            util.sleep(2500);
+
+            IWebElement btnSearch = driver.FindElement(By.Name("btnG"));
+
+            btnSearch.Click();
+
+            util.sleep(1500);
+
+            util.printMessage("################# Search: " + searchQuery + " ###########################");
+
+            for (int i = 0; i < arrPages.Length; i++)
+            {
+
+                show_results_count(arrPages[i]);
+
+            }
+
+            util.printMessage("##############################################################################");
+
+
+            }
+
+
+        private static void show_results_count(String page)
+        {
+
+            util.printMessage("--------------------------- " + page.ToUpper() + " TAB ---------------------------------------");
+            driver.FindElement(By.XPath(".//a[text()='"+ page +"']")).Click();
+            util.sleep(3000);
+            IWebElement results = driver.FindElement(By.Id("resultStats"));
+            util.printMessage("Results: " + util.getNumber(results.Text));
+            util.printMessage("------------------------------------------------------------------------------");
+
+        }
+        
+        static void Main(String[] args)
+        {
+            // TODO Auto-generated method stub
+
+            driver = new FirefoxDriver();
+
+            String[] arrSearches = { "puppies", "rats" };
+            String[] arrPages = { "Vídeos", "Noticias" };
+	    
+	         for (int i = 0; i<arrSearches.Length; i++) {
+
+                   doSearch(arrSearches[i], arrPages);
+
+             }
+
+            driver.Close();
+
+	    }
+
+    }
+    
+}
---
+++ Util.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace tarea.test
+{
+    class Util
+    {
+
+        public void sleep(int millis)
+        {
+
+            Thread.Sleep(millis);
+
+        }
+
+        public void printMessage(String message)
+        {
+
+            Console.WriteLine(message);
+
+        }
+
+        public String getNumber(String result)
+        {
+
+            bool flag = true;
+
+            String number = result.Substring(0, result.IndexOf('(') - 1);
+
+            while (flag)
+            {
+
+                number = number.Substring(number.IndexOf(" ") + 1, number.Length);
+
+                if (Char.IsDigit(number[0]))
+                {
+
+                    flag = false;
+
+                }
+
+            }
+
+            return number.Substring(0, number.IndexOf(" "));
+
+        }
+
+    }
+}
