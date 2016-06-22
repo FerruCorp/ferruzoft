cea60cb7cf0aad8c9cca88a49efaa29b555b365a
Ferruzoft <Ferruzoft>
2016-06-22 15:19:35 -0500

    nuevos archivos agregados y cambios tanto html/css/js

--- Programming/javascript/firstScript.js
+++ Programming/javascript/firstScript.js
@@ -1,25 +1,3 @@
-var obj ={};
-obj.propl = 3;
-console.log(obj);
-//////////////////////////////////////////////////////////////////////////
-function log(arg){
-    console.log(arg);
+function clean() {
+    console.clear();
 }
-log.myProp = 2;
-console.log(log);
-console.dir(log);
-//////////////////////////////////////////////////////////////////////////
-var ar = [3];
-ar.myProp = 3;
-console.log(ar);
-//////////////////////////////////////////////////////////////////////////
-var obj = {};
-obj.name = 'Joe';
-console.log(obj.name);
-console.log(obj['name']);
-
-for(var i=0; i<5; i++){
-    obj['prop' + i] = i;
-}
-console.log(obj);
-http://www.webtoolkitonline.com/javascript-tester.html
